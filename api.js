11d9d815c8cd5c79bff426b7d54ff3e77441b50a
⚡️ Bilali ⚡️ <billo26085@gmail.com>
2025-01-25 22:13:50 +0500

    Update api.js

--- api.js
+++ api.js
@@ -39,6 +39,18 @@ function displayNoUserFound() {
   document.getElementById('company').textContent = 'Company: Not Available';
 }
 
+const btn = document.querySelector(".theme-switch input");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+btn.addEventListener("click", () => {
+  if (prefersDarkScheme.matches) {
+    document.documentElement.classList.toggle("light");
+  } else {
+    document.documentElement.classList.toggle("dark");
+  }
+});
+
+
 function toggleTheme() {
   const card = document.querySelector('.card');
   const body = document.body;
